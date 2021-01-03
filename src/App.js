4e71b46aa0c3e2280d6a42ccc804f9648572a5c5
ansari <faiyazqubits@gmail.com>
2021-01-03 23:32:27 +0545

    edited App file.

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Dleted Load Balancing.
+          Dleted Load Balancing. t
         </a>
       </header>
     </div>
