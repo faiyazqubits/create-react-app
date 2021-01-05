39d5633a532fd9a75c904d666440a56a6f7bbd8d
ansari <faiyazqubits@gmail.com>
2021-01-05 09:21:41 +0545

    modified buildspec file.

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Deleted Load Balancing. this is 5th December.
+          Build pipeline without CodeBuild..
         </a>
       </header>
     </div>
